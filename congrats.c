7000e54c6b1cd52cd97e1b681b70937f32017643
lojain <hazemlojain654@gmail.com>
2025-02-28 05:50:37 -0800

    Hello Github

---
+++ congrats.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+
+  
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+ 
+    printf("Congrats %s, You Learnt How To Use GitHub!\n", name);
+
+    return 0;
+}
