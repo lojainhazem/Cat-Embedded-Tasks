757296d09a25d2d1db7027ac887f89c8c1b9c3e1
lojain <hazemlojain654@gmail.com>
2025-03-21 12:41:15 -0700

    Added Problem Set 3 solutions inside a folder

---
+++ Problem_Set_3/congrats.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+
+  
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+ 
+    printf("Congrats %s, You Learnt How To Use Github!\n", name);
+
+    return 0;
+}
